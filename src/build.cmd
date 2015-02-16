e13925b2d54b134447477c33f3640f88b79c58bc
Raymond Kroon <raymond@k3n.nl>
2015-02-16 18:44:28 +0100

    werkende index pagina

--- src/build.cmd
+++ src/build.cmd
@@ -1,1 +1,2 @@
+asciidoctor -D ../ -a stylesheet=..\stylesheets\github.css -a source-highlighter=highlightjs index.adoc
 asciidoctor -D ../ -a stylesheet=..\stylesheets\github.css -a source-highlighter=highlightjs json-aanlevering.adoc
